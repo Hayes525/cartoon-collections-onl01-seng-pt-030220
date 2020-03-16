cbb1c69b6e923736b8722ebbfd8b256ab733cd2d
Jordan Hayes <nosafoundation@gmail.com>
2020-03-16 01:55:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  puts. [dwarves
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  # Your code her
+  nsuiefhu7nek
+  puts . = 
 end
 
 def long_planeteer_calls# code an argument here
